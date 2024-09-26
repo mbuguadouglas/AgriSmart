af3c7c935569025390e135e48b83e0b252a85199
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-09-26 08:03:01 +0300

    begin displaying of possible errors that occur during signup onto the webpage DOM

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
 
+  form !: FormGroup
+
+  ngOnInit(): void {
+    this.form = new FormGroup ({
+      name : new FormControl(null, Validators.required),
+      email : new FormControl(null, [Validators.required, Validators.email]),
+      password : new FormControl(null, Validators.required),
+      role : new FormControl(null, Validators.required),
+    })
+  }
+
+  onSubmit() {
+    console.log(this.form.value)
+    // this.form.reset()
+  }
 
 }
