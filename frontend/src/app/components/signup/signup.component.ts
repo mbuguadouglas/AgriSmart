d8874e38ac00c1406838b21e65ede2b6a36f4980
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-09-26 11:07:08 +0300

    finalize on styling the signup page

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -15,16 +15,18 @@ export class SignupComponent implements OnInit{
 
   ngOnInit(): void {
     this.form = new FormGroup ({
+      // name : new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
       name : new FormControl(null, Validators.required),
       email : new FormControl(null, [Validators.required, Validators.email]),
-      password : new FormControl(null, Validators.required),
+      password : new FormControl(null, [Validators.required,
+                                        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,10}$')]),
       role : new FormControl(null, Validators.required),
     })
   }
 
   onSubmit() {
     console.log(this.form.value)
-    // this.form.reset()
+    this.form.reset()
   }
 
 }
